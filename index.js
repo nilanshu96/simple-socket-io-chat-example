6f2842043dadd2d704f640596b52f5634f1182eb
Nilanshu V Rajmane <rajmanenilanshu@gmail.com>
2021-05-15 21:08:22 +0530

    broadcasted message to every user using emit

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
         console.log('user disconnected');
-    })
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
 })
 
 server.listen(3000, () => {
